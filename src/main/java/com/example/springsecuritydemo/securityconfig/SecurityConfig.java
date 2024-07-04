8e0d6c6ee6a3cf9a8935b88fa73aa1328c7bc0ad
Sandeep Nyoupane <65792729+SandeepN97@users.noreply.github.com>
2024-07-04 12:55:01 -0500

    configured database for roled based authentication using db

--- src/main/java/com/example/springsecuritydemo/securityconfig/SecurityConfig.java
+++ src/main/java/com/example/springsecuritydemo/securityconfig/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,14 +21,18 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
 
     @Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests((request) -> request.anyRequest().authenticated());
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((request) ->
+               request.requestMatchers("/h2-console/**").permitAll().anyRequest().authenticated());
         http.sessionManagement((session
                 -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)));
         //http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
+        http.headers(headers-> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
+        http.csrf(csrf -> csrf.disable());
         return http.build();
     }
+
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails user1 = User.withUsername("user1")
