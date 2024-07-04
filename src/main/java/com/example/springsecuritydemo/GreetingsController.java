447ae471765058bf688159fbb0cc76a4717811b2
Sandeep Nyoupane <65792729+SandeepN97@users.noreply.github.com>
2024-07-04 10:35:26 -0500

    Spring Security users creation using InMemory Details

---
+++ src/main/java/com/example/springsecuritydemo/GreetingsController.java
@@ -0,0 +1,12 @@
+package com.example.springsecuritydemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+    @GetMapping("/greet")
+    public String greet() {
+        return "Hello, World!";
+    }
+}
---
+++ src/main/java/com/example/springsecuritydemo/securityconfig/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.example.springsecuritydemo.securityconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests((request) -> request.anyRequest().authenticated());
+        http.sessionManagement((session
+                -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)));
+        //http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user1 = User.withUsername("user1")
+                .password("{noop}user1")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withUsername("admin")
+                .password("{noop}admin")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, admin);
+    }
+}
