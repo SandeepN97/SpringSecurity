8c35c5a5d9922c3157d4e674f63f3c7e2245ae97
Sandeep Nyoupane <65792729+SandeepN97@users.noreply.github.com>
2024-07-04 11:02:45 -0500

    added roles based security annotation @PreAuthorize

--- src/main/java/com/example/springsecuritydemo/GreetingsController.java
+++ src/main/java/com/example/springsecuritydemo/GreetingsController.java
@@ -1,5 +1,6 @@
 package com.example.springsecuritydemo;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +10,16 @@ public class GreetingsController {
     public String greet() {
         return "Hello, World!";
     }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/user")
+    public String userEndPoint() {
+        return "hello user";
+    }
+
+    @GetMapping("/admin")
+    public String adminEndPoint() {
+        return "hello admin";
+    }
+
 }
